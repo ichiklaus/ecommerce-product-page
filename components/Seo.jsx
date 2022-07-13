0d8b819fca69030f3ab58e0f89bd765e6192a17b
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-07-13 15:23:14 -0500

    Reestructure SEO component

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -1,12 +1,10 @@
-import Head from 'next/head';
 import data from '../data';
 
 const Seo = () => {
   const { title, author, description } = data;
   return (
-    <Head>
+    <>
       <title>{title}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={description} />
       <meta name="author" content={author.name} />
       <meta name="og:type" content="website" />
@@ -16,7 +14,7 @@ const Seo = () => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:site" content="@ichiklaus" />
-    </Head>
+    </>
   );
 };
 
