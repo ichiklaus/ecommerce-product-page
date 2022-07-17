b51713f04eb82fca54e1f6be8eb6cfb40b2f9440
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-07-16 19:49:45 -0500

    disable temporarily

--- components/Cards/Cart.jsx
+++ components/Cards/Cart.jsx
@@ -1,84 +1,88 @@
-import Image from 'next/image';
-import { useMemo } from 'react';
-import Button from '../Actions/Button';
-import styles from '../../styles/Cart.module.css';
+// import Image from 'next/image';
+// import { useMemo } from 'react';
+// import Button from '../Actions/Button';
+// import styles from '../../styles/Cart.module.css';
 
-export default function Checkout({
-  productQuantity,
-  setProductQuantity,
-  display,
-}) {
-  // Importing product metadata
-  const {
-    'fall-limited-sneakers': {
-      'product-name': productName,
-      'product-price': productPrice,
-    },
-  } = require('../../data.json');
+// export default function Checkout({
+//   productQuantity,
+//   setProductQuantity,
+//   display,
+// }) {
+//   // Importing product metadata
+//   const {
+//     'fall-limited-sneakers': {
+//       'product-name': productName,
+//       'product-price': productPrice,
+//       'image-product-path': {
+//         'product-thumbnail': { 'image-product-1': imageProductThumbnail },
+//       },
+//     },
+//   } = require('../../products.json');
 
-  // Importing icons metadata
-  const {
-    icons: { 'icon-delete': iconDelete },
-    'fall-limited-sneakers': {
-      'image-product-path': {
-        'product-thumbnail': { 'image-product-1': imageProductThumbnail },
-      },
-    },
-  } = require('../../data.json');
+//   // Importing icons metadata
+//   const {
+//     icons: { 'icon-delete': iconDelete },
+//   } = require('../../data.json');
 
-  function computeTotalPrice(productPrice, productQuantity) {
-    let calcTotalPrice = Number(productPrice) * Number(productQuantity);
-    return String(Math.round((calcTotalPrice * 100) / 100).toFixed(2));
-  }
+//   function computeTotalPrice(productPrice, productQuantity) {
+//     let calcTotalPrice = Number(productPrice) * Number(productQuantity);
+//     return String(Math.round((calcTotalPrice * 100) / 100).toFixed(2));
+//   }
 
-  const totalPrice = useMemo(
-    () => computeTotalPrice(productPrice, productQuantity),
-    [productPrice, productQuantity],
-  );
+//   const totalPrice = useMemo(
+//     () => computeTotalPrice(productPrice, productQuantity),
+//     [productPrice, productQuantity],
+//   );
 
-  return (
-    <div
-      className={`${styles.wrapper} ${
-        !display ? styles.hidden : styles.visible
-      }`}
-    >
-      <p>Cart</p>
-      {productQuantity !== 0 ? (
-        <div className={styles.container}>
-          <div className={`${styles.content} ${styles.contentLayout}`}>
-            <Image
-              className={styles.previewItem}
-              src={imageProductThumbnail}
-              alt="Product preview"
-              width={56}
-              height={56}
-            />
-            <div className={styles.cartInfo}>
-              <p>{productName}</p>
-              <p>
-                <span>{productPrice}</span>
-                <span> x {productQuantity} </span>
-                <span>${totalPrice}</span>
-              </p>
-            </div>
-            <div
-              className={styles.deleteIcon}
-              onClick={() => setProductQuantity(0)}
-            >
-              <object
-                data={iconDelete}
-                type="image/svg+xml"
-                aria-label="delete"
-              ></object>
-            </div>
-          </div>
-          <Button text={'Checkout'} />
-        </div>
-      ) : (
-        <div className={`${styles.content} ${styles.emptyWarning}`}>
-          <p className={styles.emptyText}>Your cart is empty.</p>
-        </div>
-      )}
-    </div>
-  );
+//   return (
+//     <div
+//       className={`${styles.wrapper} ${
+//         !display ? styles.hidden : styles.visible
+//       }`}
+//     >
+//       <p>Cart</p>
+//       {productQuantity !== 0 ? (
+//         <div className={styles.container}>
+//           <div className={`${styles.content} ${styles.contentLayout}`}>
+//             <Image
+//               className={styles.previewItem}
+//               src={imageProductThumbnail}
+//               alt="Product preview"
+//               width={56}
+//               height={56}
+//             />
+//             <div className={styles.cartInfo}>
+//               <p>{productName}</p>
+//               <p>
+//                 <span>{productPrice}</span>
+//                 <span> x {productQuantity} </span>
+//                 <span>${totalPrice}</span>
+//               </p>
+//             </div>
+//             <div
+//               className={styles.deleteIcon}
+//               onClick={() => setProductQuantity(0)}
+//             >
+//               <object
+//                 data={iconDelete}
+//                 type="image/svg+xml"
+//                 aria-label="delete"
+//               ></object>
+//             </div>
+//           </div>
+//           <Button text={'Checkout'} />
+//         </div>
+//       ) : (
+//         <div className={`${styles.content} ${styles.emptyWarning}`}>
+//           <p className={styles.emptyText}>Your cart is empty.</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+const Cart = () => {
+  return ( <div></div> );
 }
+ 
+export default Cart;
