43f6460632d1aff0307f8eb8ff7848c486316c3d
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-07-19 14:37:16 -0500

    Change api url

--- libs/loadProducts.js
+++ libs/loadProducts.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export async function loadProductsData() {
   try {
-    const response = await axios.get('http://127.0.0.1:5000/products');
+    // const response = await axios.get('http://127.0.0.1:5000/api/products');
+    const response = await axios.get('https://ichiklaus-ecommerce-shop-api.herokuapp.com/api/products');
     const data = await response.data;
     return data;
   } catch (error) {
